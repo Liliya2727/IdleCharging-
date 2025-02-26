3aa8ca2798166a69d653789fbf43d900dd1d7817
Liliya <76985102+Liliya2727@users.noreply.github.com>
2025-02-26 23:59:04 +0700

    updatee installer

--- install.sh
+++ install.sh
@@ -8,15 +8,13 @@ REPLACE="
 "
 sleep 2
 ui_print
-ui_print "IdleCharging!!"
+ui_print "      IdleCharging!!       "
 ui_print 
-ui_print "by : @Zexshia!"
-ui_print
+ui_print "- by : @Zexshia!"
+ui_print "- Limit : 90%!"
 sleep 0.2
 ui_print "Device : $(getprop ro.product.board) "
-
 sleep 2
-ui_print
 ui_print "- Extracting module files"
 unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 unzip -o "$ZIPFILE" 'service.sh' -d "$MODPATH" >&2
